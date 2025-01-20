67885c91653132a2101333577a9ac5200a8bbe7e
Høussaine Amzil <houssaineamzil18@gmail.com>
2025-01-20 20:11:25 +0100

    new: packages installed
    In this commit I added the packages we discussed last meeting, and I set up a folder structure that we can keep or change later after more discussions, I left some simple documentation about my folder approach.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import "@/styles/globals.css"
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+import { Routes } from "@generouted/react-router"
+import { ThemeProvider } from "next-themes"
+import { createRoot } from "react-dom/client"
+import { StrictMode } from 'react'
+
+const container = document.getElementById("root")!
+createRoot(container).render(
+  <StrictMode><ThemeProvider>
+    <Routes />
+  </ThemeProvider></StrictMode>
 )
+
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import generouted from "@generouted/react-router/plugin"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
+import svgr from 'vite-plugin-svgr'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+	plugins: [react(), generouted(), svgr()],
+	resolve: { alias: { "@": "/src" } }
 })
