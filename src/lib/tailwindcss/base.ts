56df03f587b20443866a316653859411a4cec4ba
Høussaine Amzil <houssaineamzil18@gmail.com>
2025-01-28 00:04:53 +0100

    Update base.ts
    I fixed the span calculation to use `grid.desktop.columns` instead if the hardcoded `12`

--- src/lib/tailwindcss/base.ts
+++ src/lib/tailwindcss/base.ts
@@ -3,6 +3,5 @@ import { grid } from "./grid"
 export const base = {
   base: 4,
   span:
-    (grid.desktop.container - grid.desktop.gap * (grid.desktop.columns - 1)) /
-    12
+    (grid.desktop.container - grid.desktop.gap * (grid.desktop.columns - 1)) / grid.desktop.columns
 }
