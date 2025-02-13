7287ac0bd79045a1ac91f7e2e70b503c3f1140c3
Høussaine Amzil <houssaineamzil18@gmail.com>
2025-02-13 21:40:00 +0100

    fix - dynamic container
    this commit added dynamism to the container, now we relay on the margin to dynamically calculate the container width, all while have having a maximum width set to1280px by default

---
+++ src/lib/tailwindcss/types.ts
@@ -0,0 +1,21 @@
+export interface GridVars {
+  maxWidth?: number
+
+  desktop: {
+    gutter: number
+    columns: number
+    margin: number
+  }
+
+  tablet: {
+    gutter: number
+    columns: number
+    margin: number
+  }
+	
+  phone: {
+    gutter: number
+    columns: number
+    margin: number
+  }
+}
